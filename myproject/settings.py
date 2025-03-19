46b93635ccd31d39d1e16d11354fd30c4820ae6a
Jhanvi Patel <jhanvi1204@gmail.com>
2025-03-19 18:15:54 +0530

    implemented swagger for testing

--- myproject/settings.py
+++ myproject/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',  # drf_yasg for Swagger 
     'rest_framework',
     'authentication',
 ]
